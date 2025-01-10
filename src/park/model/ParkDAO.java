7a71d7ad5eea98d1f6de9694a35a6aa0d16ffcd2
HyeJinSeok <u3afguu@naver.com>
2025-01-10 14:45:16 +0900

    삭제 로직

--- src/park/model/ParkDAO.java
+++ src/park/model/ParkDAO.java
@@ -1,5 +1,30 @@
 package park.model;
 
-public class ParkDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import park.model.util.DBUtil;
+
+public class ParkDAO {
+	
+	// 공원 정보 삭제
+	public static boolean deleteParkInfo(int num) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("delete from seoul_main_park where num=?");
+			pstmt.setInt(1, num);
+			int result = pstmt.executeUpdate();
+			if (result == 1) {
+				return true;
+			}
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
+		return false;
+}
+	
 }
