729b6ca5da45c8d14cb27412989ec525e8aeb3de
PleaseErwin <rosec77@sookmyung.ac.kr>
2025-01-10 14:44:53 +0900

    FEAT : ParkDAO update

--- src/park/model/ParkDAO.java
+++ src/park/model/ParkDAO.java
@@ -4,10 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import park.model.dto.ParkDTO;
 import park.model.util.DBUtil;
 
 public class ParkDAO {
-	
+
 	// 수정
 	// location으로 주요식물 수정하기
 	public static boolean updateParkinfo(String principalSpecies, String location) throws SQLException {
@@ -15,11 +16,38 @@ public class ParkDAO {
 		PreparedStatement pstmt = null;
 		try {
 			con = DBUtil.getConnection();
-			pstmt = con.prepareStatement("update Seoul_Main_Park set principalSpecies=? where location=?");
+			pstmt = con.prepareStatement("update seoul_main_park set principalSpecies=? where location=?");
 			pstmt.setString(1, principalSpecies);
 			pstmt.setString(2, location);
 
 			int result = pstmt.executeUpdate();
+			if (result == 1) {
+				return true;
+			}
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
+		return false;
+	}
+
+	// 공원 정보 추가(insert)
+	public static boolean addParkInfo(ParkDTO park) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("insert into seoul_main_park values(?, ?, ?, ?, ?, ?, ?, ?)");
+			pstmt.setInt(1, park.getNum());
+			pstmt.setString(2, park.getParkName());
+			pstmt.setString(3, park.getOpeningDate());
+			pstmt.setString(4, park.getPrincipalSpecies());
+			pstmt.setString(5, park.getDirections());
+			pstmt.setString(6, park.getLocation());
+			pstmt.setString(7, park.getOfficeNumber());
+			pstmt.setString(8, park.getKeyFacilities());
+
+			int result = pstmt.executeUpdate();
+
 			if (result == 1) {
 				return true;
 			}
